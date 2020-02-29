f45b049cfa6b92a9891526693345a17aa7d8a2a3
Marcelo Poblete <marcelopoblette@gmail.com>
2020-02-29 11:21:48 -0300

    components creation

--- clase2/src/app/app.module.ts
+++ clase2/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutComponent } from './components/about/about.component';
 import { AnimalComponent } from './components/animal/animal.component';
 import { AnimalTarjetaComponent } from './components/animal-tarjeta/animal-tarjeta.component';
+import { AnimalesComponent } from './components/animales/animales.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     AnimalComponent,
-    AnimalTarjetaComponent
+    AnimalTarjetaComponent,
+    AnimalesComponent,
+    BuscadorComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
