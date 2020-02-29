0793978ad99d3263069b4af2ebe75c3f1cc9bdc4
Marcelo Poblete <marcelopoblette@gmail.com>
2020-02-29 11:05:07 -0300

    components creation

--- clase2/src/app/app.module.ts
+++ clase2/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './components/about/about.component';
+import { AnimalComponent } from './components/animal/animal.component';
+import { AnimalTarjetaComponent } from './components/animal-tarjeta/animal-tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AnimalComponent,
+    AnimalTarjetaComponent
   ],
   imports: [
     BrowserModule,
---
+++ clase2/src/app/components/animal-tarjeta/animal-tarjeta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-tarjeta',
+  templateUrl: './animal-tarjeta.component.html',
+  styleUrls: ['./animal-tarjeta.component.css']
+})
+export class AnimalTarjetaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
