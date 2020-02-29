06b564d0475490e1fc99b635f6f0ae1763f6184a
Marcelo Poblete <marcelopoblette@gmail.com>
2020-02-29 11:41:04 -0300

    instanciacion de rutas

--- clase2/src/app/app-routing.module.ts
+++ clase2/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './components/about/about.component';
+import { AnimalComponent } from './components/animal/animal.component';
+import { AnimalTarjetaComponent } from './components/animal-tarjeta/animal-tarjeta.component';
+import { AnimalesComponent } from './components/animales/animales.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'animales', component: AnimalesComponent},
+  {path: 'animal/:id', component: AnimalComponent},
+  {path: 'buscar/:termino', component: BuscadorComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+  //esta linea revisa todas las rutas y trata de hacer match
+];
 
-const routes: Routes = [];
+// export const APP_ROUting= RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- clase2/src/app/app.module.ts
+++ clase2/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import {APP_ROUTING } FROM '.app.routes';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
